770608bd19777a396759c597d3a972cfd15a1249
weiqiang.li <weiqiang.li@ucarinc.com>
2018-11-19 13:47:08 +0800

    feat(module): 后台管理前端初始化
    vue+elementui

---
+++ bucket-boot-admin/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            redirect: '/dashboard'
+        },
+        {
+            path: '/',
+            component: resolve => require(['../components/common/Home.vue'], resolve),
+            meta: { title: '自述文件' },
+            children:[
+                {
+                    path: '/dashboard',
+                    component: resolve => require(['../components/page/Dashboard.vue'], resolve),
+                    meta: { title: '系统首页' }
+                }
+            ]
+        },
+        {
+            path: '/login',
+            component: resolve => require(['../components/page/Login.vue'], resolve)
+        },
+        {
+            path: '*',
+            redirect: '/404'
+        }
+    ]
+})
---
+++ bucket-boot-admin/src/utils/api.js
@@ -0,0 +1,81 @@
+import axios from 'axios'
+import { MessageBox } from 'element-ui';
+
+let base = 'http://10.104.111.89:18080';
+export const postRequest = (url, params) => {
+    return axios({
+        method: 'post',
+        url: `${base}${url}`,
+        data: params,
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        }
+    }).catch(function (error) {
+        MessageBox.alert(error.response.data.message, {
+            confirmButtonText: '确定',
+            callback: action => {
+            }
+        });
+    });;
+}
+export const uploadFileRequest = (url, params) => {
+    return axios({
+        method: 'post',
+        url: `${base}${url}`,
+        data: params,
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    }).catch(function (error) {
+        MessageBox.alert(error.response.data.message, {
+            confirmButtonText: '确定',
+            callback: action => {
+            }
+        });
+    });
+}
+export const putRequest = (url, params) => {
+    return axios({
+        method: 'put',
+        url: `${base}${url}`,
+        data: params,
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        }
+    });
+}
+export const deleteRequest = (url) => {
+    return axios({
+        method: 'delete',
+        url: `${base}${url}`
+    }).catch(function (error) {
+        MessageBox.alert(error.response.data.message, {
+            confirmButtonText: '确定',
+            callback: action => {
+            }
+        });
+    });
+}
+export const getRequest = (url, params) => {
+    return axios({
+        method: 'get',
+        params: params,
+        transformRequest: [function (data) {
+            let ret = ''
+            for (let it in data) {
+                ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+            }
+            return ret
+        }],
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        url: `${base}${url}`
+    }).catch(function (error) {
+        MessageBox.alert(error.response.data.message, {
+            confirmButtonText: '确定',
+            callback: action => {
+            }
+        });
+    });
+}
